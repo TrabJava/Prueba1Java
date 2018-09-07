708a37c76af65d18bda37d333704c1ad333a7263
TrabJava <42870972+TrabJava@users.noreply.github.com>
2018-09-07 11:41:56 -0300

    aloha

---
+++ src/java/interfaces/metodosCrud.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+
+import java.util.List;
+
+/**
+ *
+ * @author duoc
+ */
+public interface metodosCrud<Generico> {
+    //Metodos CRUD
+    public boolean agregar (Generico g);
+    public boolean actualizar (Generico g);
+    public boolean eliminar (Object o);
+    public Generico buscar (Object o);
+    public List<Generico> listarTodo  (Object g);
+    
+    
+    
+}
